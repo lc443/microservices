8c241161d5b2e2105125513f1c5eb79cfb43b34f
Leron Charles <lc443@njit.edu>
2024-01-20 01:23:22 -0500

    start with docker compose

---
+++ README.md
@@ -0,0 +1,5 @@
+## pushing microservices images to docker 
+run mvn clean compile jib:build 
+
+## running kafka locally 
+o run this in terminal use command docker-compose up -d
--- product-service/src/test/java/com/leron/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/leron/productservice/ProductServiceApplicationTests.java
@@ -1,6 +1,7 @@
 package com.leron.productservice;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
